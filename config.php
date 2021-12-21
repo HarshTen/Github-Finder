a7c2b748ff2d5826eddb7c14fffe9d73a427131c
Harshavardhan <harshgavhale88@gmail.com>
2021-12-21 19:32:25 +0530

    sample config file for confidential creds sh: wq: command not found

---
+++ config.php
@@ -0,0 +1,45 @@
+<?php 
+$setup = array(
+'LIVE'=>array(
+   'database'=>array(
+     'host'=>'',
+     'username'=>'',
+     'password'=>'',
+     'database_name'=>'',
+   ),
+   'MAIL_API'=>array(
+
+     'sender'=>'',
+     'api_key'=>'',
+   )
+),
+
+'TEST'=>array(
+   'database'=>array(
+     'host'=>'localhost',
+     'username'=>'root',
+     'password'=>'',
+     'database_name'=>'rtc',
+   ),
+   'MAIL_API'=>array(
+
+     'sender'=>'',
+     'api_key'=>'',
+   )
+),
+
+);
+// SITE_MODE can be LIVE  |  TEST
+define('SITE_MODE', 'TEST');
+
+$db_args =  $setup[SITE_MODE]['database'];
+
+$con=mysqli_connect($db_args['host'],$db_args['username'],$db_args['password'],$db_args['database_name']);
+$mail_api = $setup[SITE_MODE]['MAIL_API'];
+
+// MAIL_API will have array
+define('SENDER', $mail_api['sender']);
+// MAIL_API will have array
+define('API_KEY', $mail_api['api_key']);
+
+?>
