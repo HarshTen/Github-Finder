d6ab5711dfabd62b069b1d3a799a6fcbb5b9983e
Harshavardhan <harshgavhale88@gmail.com>
2021-12-22 22:26:02 +0530

    minor changes

--- check.php
+++ check.php
@@ -1,5 +1,5 @@
 <?php
-require 'db.php';
+require 'config.php';
 if(isset($_GET['id']) && !empty($_GET['id'])){ 
 $id=mysqli_real_escape_string($con,$_GET['id']);
 mysqli_query($con,"update user set verification_status='1' where verification_token='$id'");
